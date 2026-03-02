4f3b7e481f3910d459737b75951e08b0214ffa68
Jenna Perkin <45342929+ssesses@users.noreply.github.com>
2026-03-02 10:02:06 -0500

    player RB Movement Basics

---
+++ Assets/Scripts/PlayerRBMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRBMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Vector3 InputKey;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gets Rigidbody component, checks if Rigidbody is assigned
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+            Debug.Log("Rigidbody not assigned");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Uses Unity's axis system to assign player move direction
+        InputKey = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+    //Fixed update to avoid issues of player moving faster or slower on different computers
+    void FixedUpdate()
+    {
+        //Multiplies Unity's axis system value by our move speed variable
+        rb.AddForce(InputKey * moveSpeed);
+    }
+}
