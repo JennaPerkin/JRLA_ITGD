8445de4c4d1f4d88a8dbece3c564e53f1ebe375b
Jenna Perkin <45342929+ssesses@users.noreply.github.com>
2026-04-17 08:57:21 -0400

    particle system plays when ball bounces on ground
    view scene "animation"

--- Assets/Scripts/BallBounceController.cs
+++ Assets/Scripts/BallBounceController.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Burst;
 using UnityEngine;
 
 public class BallBounceController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private AudioSource src;
+    [SerializeField] private AudioClip animStart, animBounce;
+    [SerializeField] private ParticleSystem burst;
+
+
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
+            src.clip = animStart;
+            src.Play();
             animator.SetBool("isSpaceHeld", true);
         }
-        else
+        else if (Input.GetKeyUp(KeyCode.Space))
         {
             animator.SetBool("isSpaceHeld", false);
         }
+    }
+
+    //Collision Sound Effect Method
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject != null)
+        {
+            Debug.Log("Hit Ground");
+            src.Play();
+        }
+        else
+        {
+            Debug.Log("Null Hit");
+        }
+    }*/
+
+    //Animation Event Method
+    private void PlaySound()
+    {
+        src.clip = animBounce;
+        src.Play();
+        burst.Play();
     }
 }
--- Assets/Scripts/Ren_BallController.cs
+++ Assets/Scripts/Ren_BallController.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class Ren_BallController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private AudioSource src;
+    [SerializeField] private AudioClip animBounce, animStart;
+    [SerializeField] private ParticleSystem burst;
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            src.clip = animStart;
+            src.Play();
             animator.SetBool("isSpacePressed", true);
         }
         else if(Input.GetKeyUp(KeyCode.Space))
         {
             animator.SetBool("isSpacePressed", false);
         }
+    }
+
+    void SoundPlay()
+    {
+        src.clip = animBounce;
+        src.Play();
+        burst.Play();
     }
 }
